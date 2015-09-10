6ae367845133e6f7d078f66680eb77e793499ecb
Andre Marais PC <agmarais@gmail.com>
2015-09-10 08:41:19 +0200

    graphs

--- P2/RF_subway.R
+++ P2/RF_subway.R
@@ -1,27 +1,39 @@
-install.packages('randomForest')
-install.packages('reshape2')
+
 library(reshape2)
 library(ggplot2)
-turn.data <- read.csv('C:/Udacity/Data Science/Data Visualizing/turnstile_data_master_with_weather.csv', row.names = NULL)
+turn.data <- read.csv('C:/Github/DataAnalystNanodegree/P2/Data Visualizing/turnstile_data_master_with_weather.csv', row.names = NULL)
 turn.data$X <- NULL
 
 turn.data$DATEn <- as.Date(turn.data$DATEn)
 turn.data$TIMEn <- format(strptime(as.character(turn.data$TIMEn), format = '%H:%M:%S'), format = '%H:%M:%S')
 turn.data$weekday <- weekdays(turn.data$DATEn)
+turn.data$rain <- as.factor(turn.data$rain)
+levels(turn.data$rain) <- c('No Rain', 'Rain')
+
 daily.entries <- aggregate(data = turn.data, ENTRIESn_hourly  ~ DATEn + rain, FUN = 'sum' )
-daily.entries$rain <- as.factor(daily.entries$rain)
+daily.exits <- aggregate(data = turn.data, EXITSn_hourly  ~ DATEn + rain, FUN = 'sum' )
+
+daily.commute <- merge(daily.entries, daily.exits, by = 'DATEn')
+daily.commute$rain.y <- NULL
+colnames(daily.commute) <- c('Date', 'Rain','Entries_hourly', 'Exits_hourly')
+
+graph1 <- ggplot(turn.data) + 
+  geom_histogram(aes(x = ENTRIESn_hourly, fill = rain, col = rain), binwidth=500, alpha = .65) +facet_grid(.~rain) +
+  scale_x_continuous(limits = c(0, 6000)) +
+  theme(panel.background = element_blank(),
+        legend.title = element_blank()) +
+  xlab('Hourly entries') +
+  ylab('Frequency')+
+  ggtitle('Histogram of ridership on rainy and non-rainy days')
+
+week.of.day <- aggregate(data = turn.data, ENTRIESn_hourly ~ weekday + Hour, 'sum')
+
+ggplot(week.of.day) +
+  geom_line(aes(x = Hour, y = ENTRIESn_hourly)) + 
+  facet_grid(weekday ~.)
 
 
-ggplot(daily.entries) + 
-  geom_bar(aes(x = DATEn, y = ENTRIESn_hourly, fill = rain), stat = 'identity', col = NA, alpha = .5)
 
-
-temp.agg <- aggregate(data = turn.data, ENTRIESn_hourly ~ meantempi + meanwindspdi+ rain, 'sum')
-temp.agg$rain <- as.factor(temp.agg$rain)
-ggplot(temp.agg) +
-  geom_point(aes(x = meantempi, y = meanwindspdi, size = ENTRIESn_hourly, shape = rain))
-
-ggplot(turn.data) + geom_histogram(aes(x = ENTRIESn_hourly), binwidth=1000) +facet_grid(rain ~.) + scale_x_continuous(limits = c(0, 10000))
 #
 #dcast(df_melt, year + month ~ variable, sum)
 # junk====
